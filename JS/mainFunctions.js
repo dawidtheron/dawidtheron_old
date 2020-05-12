f92265063c4afc13f6d4c0a8d0f68ea8f1bfae2d
Dawid Theron <dawidtheronsa@gmail.com>
2020-05-12 15:48:33 +0200

    commit

--- JS/mainFunctions.js
+++ JS/mainFunctions.js
@@ -1,6 +1,17 @@
-/* $(document).ready(function() {
-    var items = $("#menu1 li");
-    items.hover(function() {
-        $(this).find("i").toggle(500);
-    })
-}) */
+(function() {
+  'use strict';
+  $('.hamburger-menu').click(function (e) {
+      e.preventDefault();
+      if ($(this).hasClass('active')){
+          $(this).removeClass('active');
+          $('.menu-overlay').fadeToggle( 'fast', 'linear' );
+          $('.menu .menu-list').slideToggle( 'slow', 'swing' );
+          $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
+      } else {
+          $(this).addClass('active');
+          $('.menu-overlay').fadeToggle( 'fast', 'linear' );
+          $('.menu .menu-list').slideToggle( 'slow', 'swing' );
+          $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
+      }
+  })
+})();
