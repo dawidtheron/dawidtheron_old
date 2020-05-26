5795715064f147123250130204aeb64e1c321892
Dawid Theron <dawidtheronsa@gmail.com>
2020-05-26 14:18:47 +0200

    major design change and work in progress

--- JS/mainFunctions.js
+++ JS/mainFunctions.js
@@ -1,17 +1,4 @@
-(function() {
-  'use strict';
-  $('.hamburger-menu').click(function (e) {
-      e.preventDefault();
-      if ($(this).hasClass('active')){
-          $(this).removeClass('active');
-          $('.menu-overlay').fadeToggle( 'fast', 'linear' );
-          $('.menu .menu-list').slideToggle( 'slow', 'swing' );
-          $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
-      } else {
-          $(this).addClass('active');
-          $('.menu-overlay').fadeToggle( 'fast', 'linear' );
-          $('.menu .menu-list').slideToggle( 'slow', 'swing' );
-          $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
-      }
-  })
-})();
+$("[data-toggle='toggle']").click(function() {
+    var selector = $(this).data("target");
+    $(selector).toggleClass('in');
+});
