50377fb42647b28ce32ba764a93ab7db00545075
Dawid Theron <dawidtheronsa@gmail.com>
2020-05-29 13:22:29 +0200

    changed menu design

--- JS/mainFunctions.js
+++ JS/mainFunctions.js
@@ -1,7 +1,18 @@
 
-
-$("[data-toggle='toggle']").click(function() {
-    var selector = $(this).data("target");
-    $(selector).toggleClass('in');
-});
-
+(function() {
+    'use strict';
+    $('.hamburger-menu').click(function (e) {
+        e.preventDefault();
+        if ($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $('.menu-overlay').fadeToggle( 'fast', 'linear' );
+            $('.menu .menu-list').slideToggle( 'slow', 'swing' );
+            $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
+        } else {
+            $(this).addClass('active');
+            $('.menu-overlay').fadeToggle( 'fast', 'linear' );
+            $('.menu .menu-list').slideToggle( 'slow', 'swing' );
+            $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
+        }
+    })
+})();
