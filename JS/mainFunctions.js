01e75938f27fd686a0ba5a28c86a74ae1d24084d
Dawid Theron <dawidtheronsa@gmail.com>
2020-06-03 15:50:44 +0200

    added project to menu

--- JS/mainFunctions.js
+++ JS/mainFunctions.js
@@ -1,20 +1,26 @@
-
-(function() {
+$(document).ready(function () {
     'use strict';
-    $('.hamburger-menu').click(function (e) {
-        e.preventDefault();
-        if ($(this).hasClass('active')){
-            $(this).removeClass('active');
-            $('#splashText').css('top','50%').css('filter','blur(0px)');
-            $('.menu-overlay').fadeToggle( 'fast', 'swing' );
-            $('.menu .menu-list').slideToggle( 'slow', 'swing' );
-            $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
-        } else {
-            $(this).addClass('active');
-            $('#splashText').css('top','85%').css('filter','blur(4px)');
-            $('.menu-overlay').fadeToggle( 'fast', 'swing' );
-            $('.menu .menu-list').slideToggle( 'slow', 'swing' );
-            $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
+        $('.hamburger-menu').click(function (e) {
+            e.preventDefault();
+            if ($(this).hasClass('active')){
+                $(this).removeClass('active');
+                $('#splashText').css('top','50%').css('filter','blur(0px)');
+                $('.menu-overlay').fadeToggle( 'fast', 'swing' );
+                $('.menu .menu-list').slideToggle( 'slow', 'swing' );
+                $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
+            } else {
+                $(this).addClass('active');
+                $('#splashText').css('top','85%').css('filter','blur(4px)');
+                $('.menu-overlay').fadeToggle( 'fast', 'swing' );
+                $('.menu .menu-list').slideToggle( 'slow', 'swing' );
+                $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
         }
     })
-})();
+})
+
+
+/* <div id="content">
+<div class="col text-center" id="splashText">
+  <h1 class="display-4 text-break">Front-end Developer</h1>
+  <p class="lead text-break">freelance user interface design & front-end development by Dawid Theron</p>        
+</div> */
