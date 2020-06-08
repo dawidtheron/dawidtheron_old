752bef887f55e256002ab3792e132e9ce9df6fb0
Dawid Theron <dawidtheronsa@gmail.com>
2020-06-08 17:26:05 +0200

    added smooth menu dropdown in js

--- JS/mainFunctions.js
+++ JS/mainFunctions.js
@@ -1,20 +1,7 @@
-/* $(document).ready(function () {
-    'use strict';
-        $('.hamburger-menu').click(function (e) {
-            e.preventDefault();
-            if ($(this).hasClass('active')){
-                $(this).removeClass('active');
-                $('#splashText').css('top','50%').css('filter','blur(0px)');
-                $('.menu-overlay').fadeToggle( 'fast', 'swing' );
-                $('.menu .menu-list').slideToggle( 'slow', 'swing' );
-                $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
-            } else {
-                $(this).addClass('active');
-                $('#splashText').css('top','85%').css('filter','blur(4px)');
-                $('.menu-overlay').fadeToggle( 'fast', 'swing' );
-                $('.menu .menu-list').slideToggle( 'slow', 'swing' );
-                $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
-        }
+$(document).ready(function () {
+    const menuDropdown = $('#navbarDropdownMenuLink-projects')
+
+    $(menuDropdown).click(function () {
+        $('.dropdown-menu').slideToggle()
     })
 })
- */
