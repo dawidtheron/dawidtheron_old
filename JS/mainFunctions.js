8fb9bb982c723cbb141607314350e1d8bdf8f105
Dawid Theron <dawidtheronsa@gmail.com>
2020-06-11 18:41:37 +0200

    added enquiries section, change page content without refresh, bg images

--- JS/mainFunctions.js
+++ JS/mainFunctions.js
@@ -1,7 +1,26 @@
+
 $(document).ready(function () {
+
     const menuDropdown = $('#navbarDropdownMenuLink-projects')
 
     $(menuDropdown).click(function () {
         $('.dropdown-menu').slideToggle()
     })
-})
+});
+
+$('#content').load('frontpage.html').ready(function () {
+    $('#page-section').addClass('bg-img-frontpage');
+});
+
+$('#services').click(function () {
+    $('#content').load('services.html');
+    $('#page-section').addClass('bg-img-services').css('animation', 'none');
+});
+$('#about').click(function () {
+    $('#content').load('about.html');
+    $('#page-section').addClass('bg-img-about').css('animation', 'none');
+});
+$('#enquiries').click(function () {
+    $('#content').load('enquiries.html');
+    $('#page-section').addClass('bg-img-enquiries').css('animation', 'none');
+});
